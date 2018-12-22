8e9ae0df879b13262011617aadb6a564ccf5ed00
unknown <lgarg@Lokesh-Garg.velocity.com>
2018-12-22 17:10:58 +0530

    My First Commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+
+
+app.use((req,res, next) => {
+
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	fs.appendFile('server.log',log + '\n', (err) => {
+		if(err){
+			console.log("Unable to append to data in log file");
+		}
+	});
+	console.log(log);
+	next();
+});
+
+app.use((req,res,next) => {
+	res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req, res) => {
+	// res.send("<body> <input type='text' name='firstname' value=''/> </body>");
+	res.render('home.hbs',{
+		welcomeMessage:'Welcome to our Website. Thans for Visiting'
+	});
+});
+
+hbs.registerHelper('getCurrentYear',() => {
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+
+
+app.get('/about',(req,res) => {
+	res.render('about.hbs',{
+		pageTitle:'About Us page'
+	});
+});
+
+app.get('/bad',(req, res) => {
+	res.send({
+		errorMessage:"Unable to connect"
+	});
+});
+
+app.listen('3000',() => {
+	console.log("server is up on port 3000");
+});
